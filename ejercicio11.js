817fca537166898d7845740c863678a2787c1848
Migueks <lhlmiguel@gmail.com>
2024-07-28 00:53:43 +0200

    ejercicio 11 finalizado

---
+++ ejercicio11.js
@@ -0,0 +1,26 @@
+const movies = [
+  { title: "Inception", duration: 148 },
+  { title: "The Dark Knight", duration: 152 },
+  { title: "Interstellar", duration: 169 },
+  { title: "Dunkirk", duration: 106 },
+  { title: "The Prestige", duration: 130 },
+  { title: "Memento", duration: 113 },
+  { title: "Batman Begins", duration: 140 },
+  { title: "The Dark Knight Rises", duration: 164 },
+  { title: "Tenet", duration: 150 },
+  { title: "Insomnia", duration: 118 },
+];
+
+function averageMovieDuration(movies) {
+  let totalDuration = 0;
+  for (let i = 0; i < movies.length; i++) {
+    let movie = movies[i];
+    totalDuration += movie.duration;
+  }
+
+  let average = totalDuration / movies.length;
+  return average;
+}
+
+const averageTotal = averageMovieDuration(movies);
+console.log(`La duración promedio es: ${averageTotal}`);
